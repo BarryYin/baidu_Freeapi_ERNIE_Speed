9df1da1d7f50467eed4dae7b57dcdae2cbb10aa0
BarryYin <1509008060@qq.com>
2024-05-21 20:13:28 +0800

    0521

---
+++ get_token.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+def get_access_token():
+    """
+    使用 API Key，Secret Key 获取access_token，替换下列示例中的应用API Key、应用Secret Key
+    """
+        
+    url = "https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=APP_Key&client_secret=Secret_Key"
+    
+    payload = json.dumps("")
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+    
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response.json().get("access_token")
+
+
+token1 = get_access_token()
+print(token1)
---
+++ test.py
@@ -0,0 +1,48 @@
+
+import requests
+import json
+
+def get_access_token():
+    """
+    使用 API Key，Secret Key 获取access_token，替换下列示例中的应用API Key、应用Secret Key
+    """
+        
+    url = "https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=APP_Key&client_secret=Secret_Key"
+
+    payload = json.dumps("")
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+    
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response.json().get("access_token")
+
+
+def main():
+    """
+    url = 创建服务时填写的API地址
+    """
+
+    url = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/ernie-speed-128k?access_token=" + get_access_token()
+    
+    payload = json.dumps({
+        "messages": [
+            {
+                "role": "user",
+                "content": "你好"
+            }
+        ]
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    
+    response = requests.request("POST", url, headers=headers, data=payload)
+    
+    print(response.headers)
+    print(response.text)
+    
+
+if __name__ == '__main__':
+    main()
